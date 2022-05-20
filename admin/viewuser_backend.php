bd9f467dce40cbe9ceecb93b2cfba1dbe7b71277
Sadman Sakib <natrium213@gmail.com>
2022-05-20 23:02:29 +0600

    query is written for the process

--- admin/viewuser_backend.php
+++ admin/viewuser_backend.php
@@ -6,6 +6,11 @@ error_reporting(0);
 $link = mysqli_connect("localhost", "root", "", "tbl_blog");
 $status = 'OK';
 $content = [];
+if (mysqli_connect_errno()) {
+    $status = 'ERROR';
+    $content = mysqli_connect_error();
+    }
+$query = "SELECT * FROM tbl_member where id = '$id'";
 
 ?>
 
