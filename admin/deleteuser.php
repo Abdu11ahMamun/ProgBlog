8e6306ed9c56455dcad54cdc78321d70305e014c
Sadman Sakib <natrium213@gmail.com>
2022-05-20 23:13:24 +0600

    buttons are added for execute the codes

--- admin/deleteuser.php
+++ admin/deleteuser.php
@@ -42,6 +42,16 @@
             </ul>
         </div>
     </nav>
-       
+        <div class="container">
+    <br>
+
+
+
+	<h3 class="text-center">Delete User Profile</h3>
+    <a href="userlist.php"><input class="btn btn-success" id="home" type="button" name="" value="User List"></a> <br><br>
+		<div><button class="btn btn-danger" id="btnChange">Delete User</button></div>	
+		<p class="text-info" id="p2"></p>
+	</div>	
+	
 	</body>
 </html>
